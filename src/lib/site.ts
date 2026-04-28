e6a90bcb7eb08ea83d4a3604e0ad0a0f1a061d66
Bar Elezra <bar@albert-capital.com>
2026-04-29 00:34:56 +0300

    feat: site config constants

---
+++ src/lib/site.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+  name: "The Credit Card Pick",
+  domain: "thecreditcardpick.com",
+  url: "https://thecreditcardpick.com",
+  tagline: "Find the card you actually need.",
+  subhead: "Independent reviews, real math, no upsell games.",
+  defaultOg: "/og-default.png",
+  twitter: "@thecreditcardpick",
+  author: "The Credit Card Pick Editorial",
+};
